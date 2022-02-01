93504222d7316837754fd9149f5d8c4e082a4ffb
unknown <arthurcrowder@att.net>
2022-01-31 22:40:11 -0600

    updating the Log In form with the state and props from index

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,51 @@
-import React from 'react';
+import { useState } from 'react'
 
-const Login = () => {
+// import React from 'react';
+ 
+//  import './styles.CSS'
+
+const Login = ({ setUser }) => {
+// creating state for logging in and rendering the nav bar
+
+    const [username, setUsername] = useState('')
+
+//create handlechange for submit
+    const handleChange = e =>{
+        setUsername(e.target.value)}
+
+        const handleSubmit = e => {
+            e.preventDefault()
+            setUser(username)
+        }
+
+        // console.log('props',setUser())
+
     return (
-        <div>
-            Login
-        </div>
+
+        <form className='mx-auto border p-2 m-2' id='login-form' onSubmit=
+        {handleSubmit}>
+            <div className="mb-3">
+                <label htmlFor="exampleInputUser1" className="form-label">UserName</label>
+                <input
+                 type="text" 
+                 className="form-control" 
+                 id="exampleInputUser1" 
+                 aria-describedby="UserHelp" 
+
+                 //adding value for the username and onchange to be the handlechange
+                 value ={username}
+                 onChange={handleChange}
+                 />
+
+                <div id="UserHelp" className="form-text">We'll never share your username with anyone else.</div>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1" />
+            </div>
+
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
     );
 }
 
