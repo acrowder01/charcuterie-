41e6307dac3393e1b53d0e367d65b1720bf943ee
unknown <arthurcrowder@att.net>
2022-02-02 13:54:10 -0600

    updating folder structures

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -37,7 +37,7 @@ const Nav = () => {
 
 
             {/* <li className="nav-item">
-          <Link className="nav-link" to="#">Images</Link>
+          <Link className="nav-link" to="">Images</Link>
         </li>
           */}
 
