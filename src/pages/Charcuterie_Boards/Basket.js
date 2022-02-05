13632ce2aff98c04789c37807555dead501d09f6
unknown <arthurcrowder@att.net>
2022-02-04 21:30:38 -0600

    updating cart structure

--- src/pages/Charcuterie_Boards/Basket.js
+++ src/pages/Charcuterie_Boards/Basket.js
@@ -5,8 +5,8 @@ import index from '../Charcuterie_Boards/index'
 
 const Basket = (props) => {
     const { cartItems, onAdd, OnRemove } = props;
-    const itemsPrice = cartItems.reduce((a, c) =>a+c.price *c.qty, 0  );
-    const taxPrice = itemsPrice *0.14;
+    const itemsPrice = cartItems.reduce((a, c) => a + c.price * c.qty, 0);
+    const taxPrice = itemsPrice * 0.14;
     const shippingPrice = itemsPrice > 2000 ? 0 : 50;
     const totalPrice = itemsPrice + taxPrice + shippingPrice;
 
@@ -39,22 +39,46 @@ const Basket = (props) => {
                     </div>
 
                 </div>
-            ))}
+             ))}
 
-    {cartItems.length} !==0  && (
-    <>
-    <hr></hr>
-    <div className='row'>
-        <div className='col-2'>Items Price </div>
-        <div className='col-1 text-right'> ${itemsPrice.toFixed(2)}</div>
-    </div>
-    
-    </>
+{cartItems.length !== 0 && (
+          <>
+            <hr></hr>
+            <div className="row">
+              <div className="col-2">Items Price</div>
+              <div className="col-1 text-right">${itemsPrice.toFixed(2)}</div>
+            </div>
+            <div className="row">
+              <div className="col-2">Tax Price</div>
+              <div className="col-1 text-right">${taxPrice.toFixed(2)}</div>
+            </div>
+            <div className="row">
+              <div className="col-2">Shipping Price</div>
+              <div className="col-1 text-right">
+                ${shippingPrice.toFixed(2)}
+              </div>
+            </div>
 
+            <div className="row">
+              <div className="col-2">
+                <strong>Total Price</strong>
+              </div>
+              <div className="col-1 text-right">
+                <strong>${totalPrice.toFixed(2)}</strong>
+              </div>
+            </div>
+            <hr />
+            <div className="row">
+              <button onClick={() => alert('Implement Checkout!')}>
+                Checkout
+              </button>
+            </div>
+          </>
+        )}
 
-        </aside>
-
-    );
+      
+    </aside>
+  );
 }
 
 export default Basket;
