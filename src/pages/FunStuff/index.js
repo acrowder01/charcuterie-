950b6222ad50c41889cb6aafc6bf55523aaf9985
unknown <arthurcrowder@att.net>
2022-02-05 22:08:26 -0600

    adding joke API structure

---
+++ src/pages/FunStuff/index.js
@@ -0,0 +1,17 @@
+import { hasSelectionSupport } from '@testing-library/user-event/dist/utils';
+import React from 'react';
+import './styles.css'
+
+const FunStuff = () => {
+    return (
+        
+       <div className='container'>
+           
+           <h1 id="jokeid">Jokes - make me laugh</h1>
+           <div id="jokes" className='jokes'></div>
+           <button id="jokeBtn" className= "btn">Jokes</button>
+ </div>
+
+    )
+}
+export default FunStuff;
