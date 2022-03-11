6a721445631a31b17e5e48786c04440a6cfe9aa8
unknown <arthurcrowder@att.net>
2022-03-10 21:59:13 -0600

    updating landing page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,6 +7,9 @@ const Home = () => {
     return (
          
         <div className ="container1"> 
+
+
+
         <div id="home">
         <div id="home1">
            <h1>Welome to Ace Charcuterie Boards 
@@ -35,7 +38,7 @@ const Home = () => {
 
  Everyone loves a good charcuterie board... and really... what's not to love!? Between the meat and the cheese and the fruit and the nuts, there's guaranteed to be something on there for everyone. Which means no more stressing over the perfect appetizer! (This is it!)
 <br></br><br></br>
-<p>
+ 
   The most important elements of a charcuterie board are meats, cheeses, savory accompaniments, sweet accompaniments, and crackers.<br></br><br></br>
 <ul>
   <li> Cheeses: choose a variety of hard and soft cheese, and cheese made from different animals (cow, sheep, goat). Expert tip: always choose at least one cheese that is familiar to your guests so they feel comfortable diving right in.
@@ -59,25 +62,25 @@ Dried fruit: apricots, cherries, figs, pineapple, mango
 Sweet spreads: Fig butter (Trader Joes brand is on every board I make!), orange marmalade, blackberry jam or other sweet spreads.
 Chocolate: a few pieces of quality dark chocolate or chocolate covered nuts.</li>
 </ul>
-</p>
  
-<br></br><br></br>
+ 
+ 
  
  
   <center>No matter the occassion there is a themed board just for you.</center>
   <br></br><br></br>
    
-  <ul class="pagination pagination-lg" id="pagination">
-   <li class="page-item active" aria-current="page">
-        <span id ="pagination" class="page-link">Popular Boards ----></span>
+  <ul className="pagination pagination-lg" id="pagination">
+   <li className="page-item active" aria-current="page">
+        <span id ="pagination" className="page-link">Popular Boards ----></span>
     </li>
-    <li class="page-item"><a class="page-link" href="https://i0.wp.com/www.agoudalife.com/wp-content/uploads/bfast-charcuterie-1.jpg?">breakfast</a></li>
-    <li class="page-item"><a class="page-link" href="https://www.number-2-pencil.com/wp-content/uploads/2021/06/4th-of-July-Charcuterie-Board-1-1.jpg">4th of July</a></li>
-    <li class="page-item"><a class="page-link" href="https://www.favfamilyrecipes.com/wp-content/uploads/2020/02/Valentine-Charcuterie-Board-5-1.jpg">Candy</a></li>
-    <li class="page-item"><a class="page-link" href="https://apumpkinandaprincess.com/wp-content/uploads/2019/02/Valentines-Day-Charcuterie-Board.jpg">Valentines</a></li>
+    <li className="page-item"><a className="page-link" href="https://i0.wp.com/www.agoudalife.com/wp-content/uploads/bfast-charcuterie-1.jpg?">breakfast</a></li>
+    <li className="page-item"><a className="page-link" href="https://www.number-2-pencil.com/wp-content/uploads/2021/06/4th-of-July-Charcuterie-Board-1-1.jpg">4th of July</a></li>
+    <li className="page-item"><a className="page-link" href="https://www.favfamilyrecipes.com/wp-content/uploads/2020/02/Valentine-Charcuterie-Board-5-1.jpg">Candy</a></li>
+    <li className="page-item"><a className="page-link" href="https://apumpkinandaprincess.com/wp-content/uploads/2019/02/Valentines-Day-Charcuterie-Board.jpg">Valentines</a></li>
     
-    <li class="page-item"><a class="page-link" href="https://www.letseatcake.com/wp-content/uploads/2021/08/halloween-charcuterie-boards-3.jpg">Halloween</a></li>
-    <li class="page-item"><a class="page-link" href="https://www.tasteofhome.com/wp-content/uploads/2020/02/Birthday-board-TOHcom21_PU6005_G11_19_2b.jpg?resize=768,512">birthday</a></li>
+    <li className="page-item"><a className="page-link" href="https://www.letseatcake.com/wp-content/uploads/2021/08/halloween-charcuterie-boards-3.jpg">Halloween</a></li>
+    <li className="page-item"><a className="page-link" href="https://www.tasteofhome.com/wp-content/uploads/2020/02/Birthday-board-TOHcom21_PU6005_G11_19_2b.jpg?resize=768,512">birthday</a></li>
      
   </ul>
   
@@ -93,7 +96,7 @@ Chocolate: a few pieces of quality dark chocolate or chocolate covered nuts.</li
  
 <br></br><br></br>
 
-<p>Charcuterie Board ideas
+ Charcuterie Board ideas <br></br><br></br>
   <ul>
 <li>Fall Charcuterie: Include small pumpkins to decorate your board, use seasonal vegetables like pears, and fall themed dips like whipped caramel apple dip or fall desserts like thinly sliced pumpkin bread or mini healthy pumpkin muffins!</li>
 <li>Christmas Charcuterie: shape your meats, cheese, and crackers into the shape of a Christmas tree or include a few small Christmas treats like Chocolate Truffles or Christmas Cornflake Wreaths!  Scattered cranberries around your board add a nice a seasonal touch.</li>
@@ -103,7 +106,7 @@ Chocolate: a few pieces of quality dark chocolate or chocolate covered nuts.</li
 </ul>
 
   
-   </p>
+   
   
  
  </div>
@@ -135,31 +138,30 @@ Chocolate: a few pieces of quality dark chocolate or chocolate covered nuts.</li
   </div>
 </div>
 <br></br><br></br>
-
-<div  id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://i0.wp.com/www.asprinkleofjoy.com/wp-content/uploads/2020/02/IMG_1960-scaled.jpg?resize=1024%2C683&ssl=1" width="0%" height="400px" class="d-block w-100" alt="cheese"/>
+<div  id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="https://i0.wp.com/www.asprinkleofjoy.com/wp-content/uploads/2020/02/IMG_1960-scaled.jpg?resize=1024%2C683&ssl=1" width="0%" height="400px" className="d-block w-100" alt="cheese"/>
     </div>
-    <div class="carousel-item">
-      <img src=" https://mycharcuterie.com/wp-content/uploads/2021/03/Luau-Theme-Charcuterie-Board-04.jpg" width="80%" height="400px" class="d-block w-100" alt="night time"/>
+    <div id ="responsive" className="carousel-item">
+      <img src=" https://mycharcuterie.com/wp-content/uploads/2021/03/Luau-Theme-Charcuterie-Board-04.jpg" width="80%" height="400px" className="d-block w-100" alt="night time"/>
     </div>
-    <div class="carousel-item">
-    <img src="https://stepoutbuffalo.com/wp-content/uploads/2022/03/4.jpg" width="70%" height="400px" class="d-block w-100" alt="Spanish Tapa Board"/>
+    <div className="carousel-item">
+    <img src="https://stepoutbuffalo.com/wp-content/uploads/2022/03/4.jpg" width="70%" height="400px" className="d-block w-100" alt="Spanish Tapa Board"/>
     </div>
-    <div class="carousel-item">
-      <img src= "https://cdn.shopify.com/s/files/1/0193/2886/0224/files/67485745_519831698558672_5866187558328729600_o_1024x1024.jpg?v=1565021637" width="70%" height="400px" class="d-block w-100" alt="date night"/>
+    <div className="carousel-item">
+      <img src= "https://cdn.shopify.com/s/files/1/0193/2886/0224/files/67485745_519831698558672_5866187558328729600_o_1024x1024.jpg?v=1565021637" width="70%" height="400px" className="d-block w-100" alt="date night"/>
     </div>
 
     
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
   </button>
 </div>
  
@@ -170,3 +172,4 @@ Chocolate: a few pieces of quality dark chocolate or chocolate covered nuts.</li
 }
 
 export default Home;
+ 
